c561d1f6b956a75bbb2d2b0d34db652fd3e3ebdf
Artem Ulyanov <74565490+TransportDebuger@users.noreply.github.com>
2024-06-02 20:55:42 +0300

    Some functions added

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
+CXX = gcc
+CFLAGS = -Wall -Werror -Wextra -x c++ -std=c++17
 SOURCE = s21_matrix_err.h s21_matrix_oop.h s21_matrix_oop.cpp
+
+all: build
+
+build: clean s21_matrix.o
+
+s21_matrix.o: ${SOURCE}
+	${CXX} ${CFLAGS} -c ${SOURCE} 
 
 clang:
 	cp ../materials/linters/.clang-format .
@@ -12,3 +21,7 @@ clangf:
 
 cppcheck:
 	cppcheck --enable=all --force --suppress=missingIncludeSystem --language=c++ ${SOURCE}
+
+clean:
+	rm -rf *.gch
+	rm -rf *.o
--- s21_matrix_err.h
+++ s21_matrix_err.h
@@ -1,11 +1,13 @@
 #ifndef S21_MATRIX_ERR_H
 #define S21_MATRIX_ERR_H
 
+//Memory and access error messeges
 #define MSG_ALLOCATION_ERROR "Memory allocation error."
+#define MSG_EMPTY_MATRIX "Matrix is not present or nullptr."
 
+//Matrix size exception messeges
 #define MSG_WRONG_DIMENTION_SIZE \
   "Dimention size mismatch. The matrix dimentions must be higher or equal 0."
-
-#define MSG_EMPTY_MATRIX "Source matrix is not present or nullptr."
+#define MSG_NEQ_MATRIX_SIZE "Unequal size of matrixes."
 
 #endif
